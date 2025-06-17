e8a04353944aa55dd97474c697c8ce68fd90f533
Shahzil Shahzad <silentboys1117@gmail.com>
2025-06-17 22:27:42 +0500

    Update PortalsService.js

--- app/services/ServicesComponents/PortalsService.js
+++ app/services/ServicesComponents/PortalsService.js
@@ -2,7 +2,7 @@ import { portals } from "@/app/constant"
 import Template from "./Template"
 const PortalsService = () => {
   return (
-    <div className="SectionHolder h-[90vh] bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="portals">
+    <div className="SectionHolder h-fit bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="portals">
       <Template content={portals} title = "Portals Service"/>
      </div>
   )
