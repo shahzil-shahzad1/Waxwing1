b8af4572b85c04221831fc079611334518f2c15f
Shahzil Shahzad <silentboys1117@gmail.com>
2025-06-17 22:28:08 +0500

    Update PortfolioService.js

--- app/services/ServicesComponents/PortfolioService.js
+++ app/services/ServicesComponents/PortfolioService.js
@@ -2,7 +2,7 @@ import { portfolios } from "@/app/constant"
 import Template from "./Template"
 const PortfolioService = () => {
   return (
-    <div className="h-[90vh] bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="portfolios">
+    <div className="h-fit bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="portfolios">
       <Template content={portfolios} title = "Portfolios Service" />
     </div>
   )
