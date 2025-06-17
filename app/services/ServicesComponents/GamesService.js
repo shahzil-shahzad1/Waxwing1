e03c83a86d214110ce283f519a2c4c897aeb9c2c
Shahzil Shahzad <silentboys1117@gmail.com>
2025-06-17 22:27:21 +0500

    Update GamesService.js

--- app/services/ServicesComponents/GamesService.js
+++ app/services/ServicesComponents/GamesService.js
@@ -2,7 +2,7 @@ import { games } from "@/app/constant"
 import Template from "./Template"
 const GamesService = () => {
   return (
-    <div className="SectionHolder h-[90vh] bg-gradient-to-b from-[#000211] via-[#010312] to-[#020316]" id="games">
+    <div className="SectionHolder h-fit bg-gradient-to-b from-[#000211] via-[#010312] to-[#020316]" id="games">
 
       <Template content={games} title = "Games Service"/>
     </div>
