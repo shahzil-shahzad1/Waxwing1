00d07c3db3ad9074b9f07bd32c692359193a6e93
Shahzil Shahzad <silentboys1117@gmail.com>
2025-06-17 22:26:48 +0500

    Update EcommerceService.js

--- app/services/ServicesComponents/EcommerceService.js
+++ app/services/ServicesComponents/EcommerceService.js
@@ -2,7 +2,7 @@ import { ecommerceStores } from "@/app/constant"
 import Template from "./Template"
 const EcommerceService = () => {
   return (
-    <div className="SectionHolder h-[90vh] bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="stores">
+    <div className="SectionHolder h-fit bg-gradient-to-b from-[#020316] via-[#010312] to-[#000211]" id="stores">
         <Template content={ecommerceStores} title="Online Stores Service" />
     </div>
   )
