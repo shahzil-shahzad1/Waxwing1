2a5a3b156492125a28252ef86e98cacf9bbd4af5
Shahzil Shahzad <silentboys1117@gmail.com>
2025-06-17 22:26:23 +0500

    Update BusinessService.js

--- app/services/ServicesComponents/BusinessService.js
+++ app/services/ServicesComponents/BusinessService.js
@@ -2,7 +2,7 @@ import { businessWeb } from "@/app/constant"
 import Template from "./Template"
 const BusinessService = () => {
   return (
-    <div className="SectionHolder h-[90vh] bg-gradient-to-b from-[#000211] via-[#010312] to-[#020316]" id="busweb">
+    <div className="SectionHolder h-fit bg-gradient-to-b from-[#000211] via-[#010312] to-[#020316]" id="busweb">
         <Template content={businessWeb} title="Business Web Service" />
     </div>
   )
